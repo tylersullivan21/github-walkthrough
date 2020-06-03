3d99be236ee80ad0b4b2d881290f67964181d786
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-06-03 14:19:14 -0400

    added chalk instead

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));const chalk = require('chalk');
+const log = console.log;
+ 
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+ 
